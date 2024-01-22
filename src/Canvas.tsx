fd1d30bece843bcee4d73bf2f16867326ab66cc4
Larissa1222 <larissa.costa1222@gmail.com>
2024-01-22 13:59:59 -0300

    feat: adc funcionalidade zoom e arrasto no canvas

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -55,11 +55,51 @@ function Canvas() {
     reader.onload = () => {
       fabricCanvasRef.current.setBackgroundImage(
         reader.result,
-        fabricCanvasRef.current.renderAll.bind(fabricCanvasRef)
+        fabricCanvasRef.current.renderAll.bind(fabricCanvasRef.current)
       );
     };
     reader.readAsDataURL(file);   
   };
+  useEffect(() => {
+    fabricCanvasRef.current.on('mouse:down', function(opt) {
+      const evt = opt.e;
+      if (evt.altKey === true) {
+        fabricCanvasRef.current.isDragging = true;
+        fabricCanvasRef.current.selection = false;
+        fabricCanvasRef.current.lastPosX = evt.clientX;
+        fabricCanvasRef.current.lastPosY = evt.clientY;
+      }
+    });
+
+    fabricCanvasRef.current.on('mouse:move', function(opt) {
+      if (fabricCanvasRef.current.isDragging) {
+        const e = opt.e;
+        const vpt = fabricCanvasRef.current.viewportTransform;
+        vpt[4] += e.clientX - fabricCanvasRef.current.lastPosX;
+        vpt[5] += e.clientY - fabricCanvasRef.current.lastPosY;
+        fabricCanvasRef.current.requestRenderAll();
+        fabricCanvasRef.current.lastPosX = e.clientX;
+        fabricCanvasRef.current.lastPosY = e.clientY;
+      }
+    });
+
+    fabricCanvasRef.current.on('mouse:up', function() {
+      fabricCanvasRef.current.setViewportTransform(fabricCanvasRef.current.viewportTransform);
+      fabricCanvasRef.current.isDragging = false;
+      fabricCanvasRef.current.selection = true;
+    });
+
+    fabricCanvasRef.current.on('mouse:wheel', function(opt) {
+      const delta = opt.e.deltaY;
+      let zoom = fabricCanvasRef.current.getZoom();
+      zoom *= 0.999 ** delta;
+      if (zoom > 20) zoom = 20;
+      if (zoom < 0.01) zoom = 0.01;
+      fabricCanvasRef.current.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+      opt.e.preventDefault();
+      opt.e.stopPropagation();
+    });
+  })
 
   return (
     <div className="global">
@@ -90,6 +130,8 @@ function Canvas() {
         </button>
       </span>
       <canvas ref={canvasRef} className="canvas"></canvas>
+      <p>Pressione a tecla alt e arraste a tela com o mouse</p>
+      <p>O zoom pode ser usado com o scroll do mouse</p>
     </div>
   );
 }
